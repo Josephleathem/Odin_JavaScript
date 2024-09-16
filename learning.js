033019ba017bf1659d8d73b113962e3a6ebe3e2a
joseph <jleat2620@gmail.com>
2024-09-16 15:17:38 -0400

    I am paused at including expressions in strings from The Odin Project data types and conditionals strings section 1

--- learning.js
+++ learning.js
@@ -2,7 +2,7 @@ console.log("Hello, External")
 
 let name = "John";
 let surname = "Doe";
-
+                                   
 console.log(name);
 console.log(surname);
 
@@ -35,3 +35,40 @@ let actual = max - 13
 let percent = actual/max
 
 console.log(percent)
+
+const string = "the revolution will not be televised.";
+console.log(string);
+
+
+// const badString1 = This is a test;
+// const badString2 = 'This is a test;  These are bad string syntax you need the quotations
+// const badString3 = This is a test';
+
+const badString = string;
+console.log(badString)
+
+const single = 'Single quotes';
+const double = "Double quotes";
+const backtick = `Backtick`;
+
+console.log(single);
+console.log(double);
+console.log(backtick);
+
+// const names = "Chris";
+// console.log(names);
+// const greeting = `Hello, ${names}`;   Commented out due to duplicate const below causing error in code
+// console.log(greeting);
+
+
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `${one}${two}`;
+console.log(joined);
+
+const greeting = "Hello"; 
+const names = "Chris";
+
+console.log(greeting + ", " + names);
+
+console.log(`${greeting}, ${names} two.`);

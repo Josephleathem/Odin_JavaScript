1d0fb0d9bea81e76b3c22057c3d2faff6228f4e9
joseph <jleat2620@gmail.com>
2024-09-16 13:12:41 -0400

    Completed lesson one

--- learning.js
+++ learning.js
@@ -1,1 +1,37 @@
 console.log("Hello, External")
+
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+console.log(age);
+
+// const pi = 3.14;
+// pi = 10;
+                        // Will cause an error and stop running, nothing after this code is ran either
+// console.log(pi);
+
+console.log((3+2)-76*(1+1));
+console.log(23+97);
+console.log(1+2+3+4+5+6);
+console.log((4+6+9)/77);
+
+let a = 10
+console.log(a);
+
+console.log(9*a);
+
+let b = 7*a
+console.log(b)
+
+let max = 57 
+let actual = max - 13
+let percent = actual/max
+
+console.log(percent)

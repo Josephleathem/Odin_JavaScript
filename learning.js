6d99b02558d786fd29bfca164b8e7c979a216aa4
joseph <jleat2620@gmail.com>
2024-09-17 12:07:51 -0400

    completed data types and conditionals lesson, now to do the assignment

--- learning.js
+++ learning.js
@@ -72,3 +72,100 @@ const names = "Chris";
 console.log(greeting + ", " + names);
 
 console.log(`${greeting}, ${names} two.`);
+
+const song = "Fight the youth";
+const score = 9;
+const highscore = 10;
+const output = `I like the song ${song}. I give it a score of ${(score/highscore)*100}%.`
+
+console.log(output);
+
+const newline = `one day you finally knew
+what you had to do, and began,`;
+
+console.log(newline);
+
+
+const newlinebreak = "one day you finally knew\nwhat you had to do, and began,";
+console.log(newlinebreak);
+
+const myString = "123";
+const myNum = Number(myString);
+console.log(typeof myNum);
+
+let text = "abcdefghijklmnopqrstuvwxyz";
+let length = text.length;
+console.log(length);
+
+let text2 = "Hello World";
+let char = text2.charAt(0)
+console.log(char)
+
+let text3 = "Hello World";
+let char2 = text3[0];
+console.log(char2)
+
+let text4 = "Apple, Banana, Kiwi";
+let part = text4.slice(7, 13);
+console.log(part);
+
+let part2 = text4.slice(7);
+console.log(part2);
+
+let str = "Apple, Banana, Kiwi";
+let sub = str.substring(7, 13);
+console.log(sub);
+
+let str2 = "Apple, Banana, Kiwi";
+let sub2 = str.substr(7);
+console.log(sub2);
+
+let upper = "Hello";
+let lower = "World";
+
+console.log(upper)
+
+let upper2 = upper.toUpperCase();
+console.log(upper2)
+
+let lower2 = lower.toLowerCase();
+console.log(lower);
+console.log(lower2);
+
+let into = "Hello";
+let intro2 = "World";
+let intro3 = into.concat(" ", intro2);
+console.log(intro3);
+
+let pad = "5";
+let padded = pad.padStart(4,"0");
+console.log(padded)
+
+let pad2 = "5";
+let padded2 = pad2.padEnd(4,"0");
+console.log(padded2)
+
+let reped = "text test, ";
+
+let result = reped.repeat(2);
+console.log(result)
+
+let test = "Replace me please";
+console.log(test);
+let newTest = test.replace("me", "ME");
+console.log(newTest);
+
+let arrayTest = "1, 2, 3";
+let tesxt = arrayTest.split(",");
+console.log(tesxt);
+
+let time = 18;
+
+if (time < 10) {
+    greet = "good morning";
+} else if (time < 20) {
+    greet = "good day";
+} else {
+    greet = "good evening"
+}
+console.log(greet)
